2084468c0c5e786fddf0e9bcc21a7ee369e15187
jspieckermann <jspieckermann@web.de>
2022-03-08 08:46:36 +0100

    Added my project

---
+++ src/main/java/org/spieckermann/useradminbackend/UserAdminBackendApplication.java
@@ -0,0 +1,13 @@
+package org.spieckermann.useradminbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAdminBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserAdminBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/spieckermann/useradminbackend/UserAdminBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spieckermann.useradminbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAdminBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
