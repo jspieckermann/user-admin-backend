fa91d70fe339004201b8293641b383977ba0213b
jspieckermann <jspieckermann@web.de>
2022-03-09 18:28:11 +0100

    Implement standard flow

--- src/main/java/org/spieckermann/useradminbackend/user/UserController.java
+++ src/main/java/org/spieckermann/useradminbackend/user/UserController.java
@@ -5,11 +5,13 @@ import java.util.List;
 import javax.annotation.security.RolesAllowed;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin("*")
 public class UserController {
 	
 	@Autowired
